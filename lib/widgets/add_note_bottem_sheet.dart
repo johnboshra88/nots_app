43899f9c54ead19354dcb36126f15e97b7b214c1
johnboshra88 <johnboshra10988@gmail.com>
2024-08-27 17:23:39 +0300

    finish notes bottom sheet

--- lib/widgets/add_note_bottem_sheet.dart
+++ lib/widgets/add_note_bottem_sheet.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:nots_app/widgets/constants.dart';
+import 'package:nots_app/widgets/custom_bottom.dart';
 import 'package:nots_app/widgets/custome_text_field.dart';
 
 class AddNoteBottemSheet extends StatelessWidget {
@@ -8,22 +10,31 @@ class AddNoteBottemSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Padding(
       padding: EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
-        children: [
-          SizedBox(
-            height: 32,
-          ),
-          CustomeTextField(
-            hint: 'title',
-          ),
-          SizedBox(
-            height: 16,
-          ),
-          CustomeTextField(
-            hint: 'content',
-            maxLines: 5,
-          )
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 32,
+            ),
+            CustomeTextField(
+              hint: 'title',
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            CustomeTextField(
+              hint: 'content',
+              maxLines: 5,
+            ),
+            SizedBox(
+              height: 32,
+            ),
+            CustomBottom(),
+            SizedBox(
+              height: 16,
+            )
+          ],
+        ),
       ),
     );
   }
---
+++ lib/widgets/custom_bottom.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:nots_app/widgets/constants.dart';
+
+class CustomBottom extends StatelessWidget {
+  const CustomBottom({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      decoration: BoxDecoration(
+          color: kPrimaryColor, borderRadius: BorderRadius.circular(8)),
+      height: 50,
+      child: Center(
+        child: Text(
+          'Add',
+          style: TextStyle(
+              color: Colors.black, fontSize: 18, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
