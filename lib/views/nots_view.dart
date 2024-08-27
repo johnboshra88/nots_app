e1108ccab98bfd86bb4c1f770851cfaee226c231
johnboshra88 <johnboshra10988@gmail.com>
2024-08-27 15:52:57 +0300

    show model sheet

--- lib/views/nots_view.dart
+++ lib/views/nots_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nots_app/widgets/add_note_bottem_sheet.dart';
 import 'package:nots_app/widgets/notes_view_body.dart';
 
 class NotesView extends StatelessWidget {
@@ -7,15 +8,21 @@ class NotesView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton( ,
+      floatingActionButton: FloatingActionButton(
         backgroundColor: const Color.fromARGB(255, 6, 243, 207),
-        onPressed: () {},
-        child: Icon(
+        onPressed: () {
+          showModalBottomSheet(
+              context: context,
+              builder: (context) {
+                return const AddNoteBottemSheet();
+              });
+        },
+        child: const Icon(
           Icons.add,
           color: Colors.black,
         ),
       ),
-      body: NotsViewBody(),
+      body: const NotsViewBody(),
     );
   }
 }
