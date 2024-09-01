c2282c2c1ea3238f3d66ec3165c7da0fa227f1d0
johnboshra88 <johnboshra10988@gmail.com>
2024-09-01 19:45:58 +0300

    create cubit

--- lib/widgets/add_note_bottem_sheet.dart
+++ lib/widgets/add_note_bottem_sheet.dart
@@ -65,6 +65,7 @@ class _AddNoteFormState extends State<AddNoteForm> {
                 formKey.currentState!.save();
               } else {
                 autovalidateMode = AutovalidateMode.always;
+                setState(() {});
               }
             },
           ),
